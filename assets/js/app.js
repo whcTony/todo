73ec9a3ba7a7664046a36c1b243fe9747b9b5b6d
tony <wanghaocbjx@sina.com>
2017-09-14 11:16:35 +0800

    todo完成

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,21 @@
 angular.module('todo',[])
     
        .controller('todoCtrl',['$scope',function($scope){
+       	    /*
+             获取数据
+       	    */
+            	$scope.taskList = [];
+            	GetTasks();
+            	function GetTasks(){
+            		if(localStorage.getItem('taskList')){
+            		$scope.taskList = angular.fromJson(localStorage.getItem('taskList'))
+            		}
+            	}
+
+
+
+
+
        	/*
        添加任务
        1、 获取用户输入的任务名字
@@ -11,18 +26,22 @@ angular.module('todo',[])
 
 
        	*/
-       	$scope.taskList = [];
+       	
        	$scope.addTask = function(event){
        		//如果用户按的是回车键并且文本框中有内容
                  if(event.keyCode == 13 &&$scope.task){
                  	//将用户输入的任务名字添加到任务数组中
                  	$scope.taskList.push({
                  		name:$scope.task,
-                 		isCompleted:false
+                 		isCompleted:false,
+                 		isEditing:false
                  	});
-                 	$scope.task = ""
-                 }
+                 	$scope.task = "";
+
+                 	localStorage.setItem('taskList',angular.toJson($scope.taskList))
+
        	}
+       }
        	/*删除任务
           1、给删除按钮添加点击事件
           2、把需要删除的任务传递进入事件函数中
@@ -48,4 +67,93 @@ angular.module('todo',[])
 
             	return $scope.taskList.filter(item =>!item.isCompleted).length
             }
+             
+
+             $scope.select = 'All';
+            $scope.filterData = function(type){
+              switch(type){
+              	case 'All':
+                 $scope.filterType = '';
+                 $scope.select = 'All';
+              	break;
+
+              	case 'Active':
+                     $scope.filterType = 'false';
+                     $scope.select = 'Active';
+              	break;
+
+              	case 'Completed':
+                      $scope.filterType = 'true';
+                      $scope.select = 'Completed';
+              	break;
+              }
+            }
+
+            /*
+             清除已完成任务
+            
+            */
+            $scope.clearCompletedTask = function(){
+            	 // for(var i=0;i<$scope.taskList.length;i++){
+            	 // 	if($scope.taskList[i].isCompleted){
+              //           $scope.taskList.splice(i,1);
+              //           i--;               
+            	 // 	}
+            	 // }
+
+            	 //对数据尽心过滤，将未完成的任务留下
+            	 $scope.taskList = $scope.taskList.filter(item => !item.isCompleted)
+            }
+
+            /*批量更改任务状态*/
+            $scope.changeStatus = function(){
+            	$scope.taskList.forEach(item => item.isCompleted = $scope.status)
+            }
+
+            $scope.updataStatus = function(){
+
+            	// for(var i=0;i<$scope.taskList.length;i++){
+            	// 	if(!$scope.taskList[i].isCompleted){
+             //                $scope.status = false;
+             //                return
+            	// 	}
+            	// }
+            	// $scope.status = true;
+            	//如果未完成任务的数量是0，说明都完成了，高亮
+           $scope.status = $scope.taskList.filter(item => !item.isCompleted).length == 0
+            }
+
+            /*更改任务名称*/
+            $scope.modifyTaskName = function(task){
+            	//将所有的任务取消编辑状态
+              $scope.taskList.forEach(item =>item.isEditing =false);
+              //将当前双击的任务添加编辑状态
+               task.isEditing = true;
+            }
+                //取消编辑状态
+            $scope.cancelEditing = function(){
+            	 $scope.taskList.forEach(item =>item.isEditing =false);
+            }
+            $scope.$watch('taskList',function(){
+            	localStorage.setItem('taskList',angular.toJson($scope.taskList))
+            },true)
+
+
+       }])
+
+       .directive('inpFocus',['$timeout',function($timeout){
+       	return {
+       		restrict:'A',
+       		link:function(scope,element,attributes){
+               scope.$watch('item.isEditing',function(newvalue){
+                    
+                    if(newvalue){
+                    $timeout(function(){
+                          element[0].focus();
+                    },0)
+                    	
+                    }
+               })
+       		}
+       	}
        }])
